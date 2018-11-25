ef241c6cd956b35979569c836d099198651f0d8b
Warren Jones <warren@pencethren.org>
2018-11-25 22:42:33 +0000

    Moved jest-enzyme import into individual test.
    This makes various bits of tooling less complainy.

--- mistborn-frontend/src/components/Glyph.test.tsx
+++ mistborn-frontend/src/components/Glyph.test.tsx
@@ -1,4 +1,5 @@
 import {shallow} from 'enzyme';
+import "jest-enzyme";
 import * as React from "react";
 import Glyph from "./Glyph";
 
--- mistborn-frontend/src/setupTests.ts
+++ mistborn-frontend/src/setupTests.ts
@@ -1,5 +1,4 @@
 import * as enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
-import 'jest-enzyme';
 
 enzyme.configure({ adapter: new Adapter() });
