fcd797e7384801ef8138fba1e40d01e30d30cf5b
Warren Jones <warren@pencethren.org>
2018-11-26 22:16:45 +0000

    Added extra unit tests for Section contents.

--- mistborn-frontend/src/components/Section.test.tsx
+++ mistborn-frontend/src/components/Section.test.tsx
@@ -40,8 +40,39 @@ describe("Section", () => {
       </Section>
     );
 
-    it("has a main element containing the children", () => {
+    it("has a main element containing the child text", () => {
+      expect(section).toContainMatchingElement("main");
       expect(section.find("main")).toIncludeText(childText);
+    });
+  });
+
+  describe("With child elements", () => {
+    const title = "Section Name";
+    const glyph = "pewter";
+    const childText = "Here is some content.";
+    const secondChildText = "Here is some more content.";
+    const section = shallow(
+      <Section glyph={glyph} title={title}>
+        <div>{childText}</div>
+        <div>{secondChildText}</div>
+      </Section>
+    );
+
+    it("has a main element containing the children", () => {
+      expect(section).toContainMatchingElement("main");
+      expect(section.find("main")).toContainMatchingElement("div");
+      expect(
+        section
+          .find("main")
+          .find("div")
+          .first()
+      ).toIncludeText(childText);
+      expect(
+        section
+          .find("main")
+          .find("div")
+          .last()
+      ).toIncludeText(secondChildText);
     });
   });
 });
