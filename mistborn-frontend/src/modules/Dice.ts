f648d49f32cf81aeaed521d21f2dae4e25306d79
Warren Jones <warren@pencethren.org>
2019-03-24 18:18:50 +0000

    Add  JSDoc to Dice class.

--- mistborn-frontend/src/modules/Dice.ts
+++ mistborn-frontend/src/modules/Dice.ts
@@ -1,3 +1,10 @@
+/**
+ * Roll a number of regular dice, and return the results as an array.
+ *
+ * @param dice the number of dice to roll
+ * @param sides the number of sides each die has (default: 6)
+ * @returns an array containing the results of rolling the dice.
+ */
 export const rollMany: (dice: number, sides?: number) => number[] = (
   dice,
   sides
@@ -5,6 +12,12 @@ export const rollMany: (dice: number, sides?: number) => number[] = (
   return new Array<number>(dice).fill(0).map(() => rollOne(sides));
 };
 
+/**
+ * Roll a single (regular, fair) die and return the result.
+ *
+ * @param sides the number of sides the die has (default: 6)
+ * @returns the result of rolling the die.
+ */
 export const rollOne: (sides?: number) => number = sides => {
   return Math.floor(Math.random() * (sides || 6)) + 1;
 };
